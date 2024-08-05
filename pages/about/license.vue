524d720ca99853f89d54dacfa7b490dec0488327
Mikhail Shytsko <xfiive@mail.ru>
2024-08-06 00:00:42 +0200

    added text tags to license and testimony

--- pages/about/license.vue
+++ pages/about/license.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
+<script setup lang="ts" xmlns="http://www.w3.org/1999/html">
 
 </script>
 
 <template>
   <div class="about-license">
     <div class="license">
+      <h2><strong><span>Лицензия №33130/2003</span></strong></h2>
       <img src="/images/license.webp" alt="Лицензия №33130/2003" class="company-license">
     </div>
     <div class="testimony">
+      <h2><strong><span>Свидетельство о регистрации компании</span></strong></h2>
       <img src="/images/testimony.webp" alt="Свидетельство о регистрации компании" class="company-testimony">
     </div>
   </div>
